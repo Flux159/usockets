c89cfb0db4913647e258f91c485dbfa019a67c0b
Suyog Sonwalkar <suyog@chronotale.com>
2017-06-19 13:56:44 -0400

    Default arguments not supported by uglify.

--- index.js
+++ index.js
@@ -22,18 +22,28 @@ Socket.prototype.emitAsync = function emitAsync(event, payload) {
   }.bind(this));
 };
 
-Socket.prototype.request = function request(method, data, options = { event: 'socket.io-req', timeout: 60000 }) {
+Socket.prototype.request = function request(method, data, options) {
+  if (!options) {
+    options = {
+      event: 'socket.io-req',
+      timeout: 60000,
+    };
+  } else {
+    options.event = options.event || 'socket.io-req';
+    options.timeout = options.timeout || 60000;
+  }
+
   if (typeof method !== 'string') throw new Error('Method must be a string');
 
   let timeout;
 
-  const onDisconnect = function() {
+  const onDisconnect = function () {
     clearTimeout(timeout);
     reject(new Error('Socket disconnected'));
   };
 
-  return new Promise(function(resolve, reject) {
-    this.emit(options.event, { method, data }, function(res) {
+  return new Promise(function (resolve, reject) {
+    this.emit(options.event, { method, data }, function (res) {
       clearTimeout(timeout);
       this.removeListener('disconnect', onDisconnect);
       if (res.error) {
@@ -43,7 +53,7 @@ Socket.prototype.request = function request(method, data, options = { event: 'so
     }.bind(this));
   }.bind(this));
 
-  timeout = setTimeout(function() {
+  timeout = setTimeout(function () {
     this.removeListener('disconnect', onDisconnect);
     reject(new Error(`Socket request timeout: Exceeded ${options.timeout} msec`));
   }.bind(this), options.timeout);
@@ -51,17 +61,24 @@ Socket.prototype.request = function request(method, data, options = { event: 'so
   this.once("disconnect", onDisconnect);
 };
 
-Socket.prototype.response = function(method, callback, options = { event: 'socket.io-req' }) {
+Socket.prototype.response = function (method, callback, options) {
+  if (!options) {
+    options = {
+      event: 'socket.io-req',
+    };
+  } else {
+    options.event = options.event || 'socket.io-req';
+  }
   if (typeof method !== 'string') throw new Error('Method must be a string');
   if (typeof callback !== 'function') throw new Error('Callback must be a function');
 
-  this.on(options.event, function(req, ack) {
+  this.on(options.event, function (req, ack) {
     if (req.method !== method) return;
-    const res = function(data) {
+    const res = function (data) {
       ack({ data });
     };
-    res.error = function(err) {
-      ack({ error: JSON.stringify(err, replaceErrors)});
+    res.error = function (err) {
+      ack({ error: JSON.stringify(err, replaceErrors) });
     };
     callback(req.data, res);
   }.bind(this));
